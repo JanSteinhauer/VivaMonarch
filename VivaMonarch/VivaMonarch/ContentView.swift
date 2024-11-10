0f332b5f7a373a5468a92d76f7dbe539726c448c
Steinhauer <jas27346@lib-pclem115.austin.utexas.edu>
2024-11-09 22:11:18 -0600

    Credentials Hub added

--- VivaMonarch/VivaMonarch/ContentView.swift
+++ VivaMonarch/VivaMonarch/ContentView.swift
@@ -22,6 +22,7 @@ struct ContentView: View {
     
     let MonarchURL = "https://firebasestorage.googleapis.com/v0/b/vivamonarch-b34f3.appspot.com/o/Monarch6.mp4?alt=media&token=b8b6bb34-ce96-426e-9c03-ee2947883e94"
     let WhalesURL = "https://firebasestorage.googleapis.com/v0/b/greenshiftai.appspot.com/o/Whale%20North%20Sunset%20Final%20A.mp4?alt=media&token=68d4186f-2ccb-4740-b423-de77e0a805be"
+    let CreditsURL = "https://firebasestorage.googleapis.com/v0/b/greenshiftai.appspot.com/o/Butterflies%20Blue%20Sky_1.mp4?alt=media&token=383bcdec-db25-4d69-8120-506b8c9a1b4c"
 
 
     @Environment(\.openImmersiveSpace) var openImmersiveSpace
@@ -144,17 +145,34 @@ struct ContentView: View {
                     .padding()
 
                     VStack {
-                        Link(destination: URL(string: "https://www.instagram.com/stei.jan/")!) {
+                        Button(action: {
+                            stopAudio()
+                            viewModel.urlString = CreditsURL
+                            Task {
+                                await openCommonImmersiveSpace()
+                            }
+                        }) {
                             HStack {
                                 Image("Instagram")
                                     .resizable()
                                     .frame(width: 140, height: 140)
+                                    .cornerRadius(15)
                             }
-                            .cornerRadius(15)
                         }
                         .buttonStyle(.plain)
                     }
                     .padding()
+//                    VStack {
+//                        Link(destination: URL(string: "https://www.instagram.com/stei.jan/")!) {
+//                            HStack {
+//                                Image("Instagram")
+//                                    .resizable()
+//                                    .frame(width: 140, height: 140)
+//                            }
+//                            .cornerRadius(15)
+//                        }
+//                        .buttonStyle(.plain)
+//                    }
 
                     VStack {
                         Toggle(isOn: $showImmersiveSpaceVideoGallery) {
