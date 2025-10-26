0792c8d0600b1cef7430fc6f35c4e12c96e7412b
Jan Steinhauer <jan.steinhauer@sap.com>
2025-10-26 19:25:42 +0100

    updated creadits Video

--- VivaMonarch/VivaMonarch/Enums/Media.swift
+++ VivaMonarch/VivaMonarch/Enums/Media.swift
@@ -10,5 +10,5 @@ import Foundation
 enum Media {
     static let monarch = "https://firebasestorage.googleapis.com/v0/b/vivamonarch-b34f3.appspot.com/o/Monarch6.mp4?alt=media&token=b8b6bb34-ce96-426e-9c03-ee2947883e94"
     static let whales  = "https://firebasestorage.googleapis.com/v0/b/greenshiftai.appspot.com/o/Whale%20North%20Sunset%20Final%20A.mp4?alt=media&token=68d4186f-2ccb-4740-b423-de77e0a805be"
-    static let credits = "https://firebasestorage.googleapis.com/v0/b/greenshiftai.appspot.com/o/Butterflies%20Blue%20Sky_1.mp4?alt=media&token=383bcdec-db25-4d69-8120-506b8c9a1b4c"
+    static let credits = "https://firebasestorage.googleapis.com/v0/b/greenshiftai.appspot.com/o/Butterflies%20Blue%20Sky_2.mp4?alt=media&token=ff20d5d6-cd31-4407-90d7-5a842798489b"
 }
